309c0b79f718a4a305404f804cfd9ebe90c450db
feigao1 <78374415+feigao1@users.noreply.github.com>
2021-07-30 10:55:49 -0700

    Update samplesize_cal.R

--- samplesize_cal.R
+++ samplesize_cal.R
@@ -1,20 +1,20 @@
 source('recencyassay_code.R')
 source('setting_code.R')
-tau_set = c(1,2); R0 = 0.5; R1 = 0.15
+tau_set = c(1,2); R0 = 0.5; R1 = 0.15; alpha=0.05; beta=0.9
 #### MSM ####
 for (tau in tau_set){
-	para = SettingMSM_para(q=1, r=0.85, tau=tau, N=NULL, alpha=0.05, beta=0.9)
-	N  = samplesize_CF(para, R1 = R1, R0 = R0)
+	para = SettingMSM_para(q=1, r=0.85, tau=tau, N=NULL)
+	N  = samplesize_CF(para, R1, R0, alpha,beta)
 	N_crit = summarize_expectnum(N,para,R1) # N, N+, Nrec, N-enroll, Nevent
 	print(c(tau,N_crit))
 }
 
 ##### Simulations ######
 nrep = 10000;  set.seed(123)
-R0 = 0.5; R1 = 0.15; tau_set = c(1,2)
+tau_set = c(1,2); R0 = 0.5; R1 = 0.15; alpha=0.05; beta=0.9
 for (tau in tau_set){
-	para = SettingMSM_para(q=1, r=0.85, tau=tau, N=NULL, alpha=0.05, beta=0.9)
-	N  = samplesize_CF(para, R1 = R1, R0 = R0)
+	para = SettingMSM_para(q=1, r=0.85, tau=tau, N=NULL)
+	N  = samplesize_CF(para, R1, R0, alpha, beta)
 	Rej0 = Rej0_I = Rej1 = Rej1_I = lambda0 = lambda1_null = lambda1_alt = NULL
 	for (rep in 1:nrep){
 		data = generatedata(N,para,R=R0)
@@ -33,19 +33,20 @@ for (tau in tau_set){
 }
 
 #### Women Trial ####
+tau_set = c(1,2); R0 = 0.5; R1 = 0.15; alpha=0.05; beta=0.9
 for (tau in tau_set){
-	para = SettingWomen_para(q=1, r=0.85, tau=tau, N=NULL, alpha=0.05, beta=0.9)
-	N  = samplesize_CF(para, R1 = R1, R0 = R0)
+	para = SettingWomen_para(q=1, r=0.85, tau=tau, N=NULL)
+	N  = samplesize_CF(para, R1, R0, alpha, beta)
 	N_crit = summarize_expectnum(N,para,R1) # N, N+, Nrec, N-enroll, Nevent
 	print(c(tau,N_crit))
 }
 
 ##### Simulations ######
 nrep = 10000;  set.seed(123)
-R0 = 0.5; R1 = 0.15; tau_set = c(1,2)
+tau_set = c(1,2); R0 = 0.5; R1 = 0.15; alpha=0.05; beta=0.9
 for (tau in tau_set){
-	para = SettingWomen_para(q=1, r=0.85, tau=tau, N=NULL, alpha=0.05, beta=0.9)
-	N  = samplesize_CF(para, R1 = R1, R0 = R0)
+	para = SettingWomen_para(q=1, r=0.85, tau=tau, N=NULL)
+	N  = samplesize_CF(para, R1, R0, alpha, beta)
 	Rej0 = Rej0_I = Rej1 = Rej1_I = lambda0 = lambda1_null = lambda1_alt = NULL
 	for (rep in 1:nrep){
 		data = generatedata(N,para,R=R0)
